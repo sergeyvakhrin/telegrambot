eaa1faeb2d05e7dbbb48e02fdc02479302bdc0a7
sergeyvakhrin <parlamentsv@mail.ru>
2025-02-03 17:57:33 +0300

    add batton

--- main.py
+++ main.py
@@ -1,98 +1,91 @@
-from pathlib import Path
-
-import requests
-import telebot
-import json
 import os
-
+from pathlib import Path
 from dotenv import load_dotenv
-from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton
+import webbrowser
 
-from settings import whitelist
+import telebot
+from telebot import types
 
 BASE_DIR = Path(__file__).resolve().parent
 load_dotenv(BASE_DIR / '.env', override=True)
 
-token_api_sandbox = os.getenv('Authorization')
 bot = telebot.TeleBot(os.getenv('SECRET_KEY'))
 
 
-def white_user(user_id):
-    """ Валидация на белый список """
-    if user_id in whitelist:
-        return True
+@bot.message_handler(commands=['site', 'website'])
+def site(message):
+    """ Открывает сайт """
+    webbrowser.open('https://vc.com')
 
 
-def validated_imei(message) -> bool:
-    """ Проверяет введенные данные """
-    if white_user(message.from_user.id):
-        imei = message.text
-        if len(imei) != 15:
-            bot.send_message(message.chat.id, f"Вы ввели не верный IMEI! Должен состоять из 15-ти цифр.")
-            return False
-        for i in imei:
-            if not i.isdigit():
-                bot.send_message(message.chat.id, f"Вы ввели не верный IMEI! Должен состоять из 15-ти цифр.")
-                return False
-        return True
-    else:
-        bot.send_message(message.chat.id, 'Вы не имеете доступа к запросу!')
+@bot.message_handler(commands=['start', 'main', 'hello'])
+def main(message):
+    """ Обработка заданных команд """
+    markup = types.ReplyKeyboardMarkup()
+    btn1 = types.KeyboardButton('Перейти на сайт')
+    markup.row(btn1)
+    btn2 = types.KeyboardButton('Удалить фото')
+    btn3 = types.KeyboardButton('Изменить текст')
+    markup.row(btn2, btn3)
+
+    file = open('./pic.jpg', 'rb')
+    bot.send_photo(message.chat.id, file, reply_markup=markup)
+
+    bot.send_message(message.chat.id, 'Привет!', reply_markup=markup)
+
+    bot.register_next_step_handler(message, on_click)
+
+def on_click(message):
+    """ Будет выполняться первой так как объявлена как register_next_step_handler """
+    if message.text == "Перейти на сайт":
+        bot.send_message(message.chat.id, 'Website is open')
+    elif message.text == "Удалить фото":
+        bot.send_message(message.chat.id, 'Photo is deleted')
+    elif message.text == "Изменить текст":
+        bot.send_message(message.chat.id, 'Text was edited')
 
 
-def response_imei(message):
-    """ Делаем API запрос """
-    imei = message.text
-    url = "https://api.imeicheck.net/v1/checks"
-    payload = json.dumps({
-        "deviceId": imei,
-        "serviceId": 12
-    })
-    headers = {
-        'Authorization': token_api_sandbox,
-        'Accept-Language': 'en',
-        'Content-Type': 'application/json'
-    }
-    response = requests.request("POST", url, headers=headers, data=payload)
-    response = json.loads(response.text)
-    if response.get('properties'):
-        response = response['properties']
-        bot.send_message(message.chat.id, f'Модель {response['deviceName']}\n'
-                                          f'IMEI2 {response['imei2']}\n'
-                                          f'Серийный номер {response['serial']}')
-    else:
-        bot.reply_to(message, "Введен не существующий IMEI")
+# @bot.message_handler(commands=['start', 'main', 'hello'])
+# def main(message):
+#     """ Обработка заданных команд """
+#     bot.send_message(message.chat.id, f'Привет, {message.from_user.first_name} {message.from_user.last_name}')
 
 
-def process_imei(message) -> None:
-    """ Выводит данные в Телеграм """
-    imei = message.text
-    if validated_imei(message):
-        bot.send_message(message.chat.id, f"Вы ввели: {imei}")
-        response_imei(message)
+@bot.message_handler(commands=['help'])
+def main(message):
+    """ Обработка заданных команд """
+    bot.send_message(message.chat.id, '<b>Help</b> <em><u>information</u></em>', parse_mode='html')
 
 
+@bot.message_handler(content_types=['photo', 'audio'])
+def get_photo(message):
+    """ Обработка полученных фото или аудио """
+    markup = types.InlineKeyboardMarkup()
+    btn1 = types.InlineKeyboardButton('Перейти на сайт', url='https://yandex.ru')
+    markup.row(btn1)
+    btn2 = types.InlineKeyboardButton('Удалить фото', callback_data='delete')
+    btn3 = types.InlineKeyboardButton('Изменить текст', callback_data='edit')
+    markup.row(btn2, btn3)
+    bot.reply_to(message, 'Какое красивое фото!', reply_markup=markup)
 
-@bot.message_handler(commands=['start'])
-def start(message):
-    markup = InlineKeyboardMarkup()
-    markup.add(InlineKeyboardButton("Начать", callback_data="start"))
-    bot.send_message(message.chat.id, "Привет!", reply_markup=markup)
 
+@bot.callback_query_handler(func=lambda callback: True)
+def callback_message(callback):
+    if callback.data == 'delete':
+        bot.delete_message(callback.message.chat.id, callback.message.message_id - 1)
+    elif callback.data == 'edit':
+        bot.edit_message_text('Edit text', callback.message.chat.id, callback.message.message_id)
 
-@bot.callback_query_handler(func=lambda call: True)
-def callback(call):
-    if call.data == "start":
-        bot.send_message(call.message.chat.id, "Введите IMEI:")
-        bot.register_next_step_handler(call.message, process_imei)
 
 
 @bot.message_handler()
-def spase_input(message):
-    """ Игнорируем start """
-    imei = message.text
-    if validated_imei(message):
-        bot.send_message(message.chat.id, f"Вы ввели: {imei}")
-        response_imei(message)
+def info(message):
+    """ Обработка вводимой информации """
+    if message.text.lower() == 'id':
+        bot.reply_to(message, f'ID: {message.from_user.id}')
+    if message.text.lower() == 'привет':
+        bot.send_message(message.chat.id, f'Привет, {message.from_user.first_name} {message.from_user.last_name}')
 
 
-bot.polling(none_stop=True)
+
+bot.polling(non_stop=True)
