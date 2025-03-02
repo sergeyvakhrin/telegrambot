ccff6d680c54f59c32cdeefc289d999239b564bf
sergeyvakhrin <parlamentsv@mail.ru>
2025-03-02 18:17:23 +0300

    callback

--- bot.py
+++ bot.py
@@ -1,11 +1,14 @@
 import asyncio
 import logging
 import os
+from contextlib import suppress
 from datetime import datetime
+from random import randint
 
 from aiogram import Bot, Dispatcher, types, F, html
 from aiogram.client.default import DefaultBotProperties
 from aiogram.enums import ParseMode
+from aiogram.exceptions import TelegramBadRequest
 from aiogram.filters.command import Command, CommandObject
 from aiogram.types import Message
 from aiogram.utils.formatting import Text
@@ -13,6 +16,7 @@ from aiogram.utils.formatting import (
     Bold, as_list, as_marked_section, as_key_value, HashTag
 )
 from aiogram.types import FSInputFile, URLInputFile, BufferedInputFile
+from aiogram.utils.keyboard import ReplyKeyboardBuilder, InlineKeyboardBuilder
 from aiogram.utils.markdown import hide_link
 from aiogram.utils.media_group import MediaGroupBuilder
 
@@ -33,9 +37,252 @@ bot = Bot(
 dp = Dispatcher()
 
 # Хэндлер на команду /start
-@dp.message(F.text, Command("start"))
+# @dp.message(F.text, Command("start"))
+# async def cmd_start(message: types.Message):
+#     await message.answer("Сообщение с <u>HTML-разметкой</u>", parse_mode=ParseMode.HTML)
+
+
+# @dp.message(Command("start"))
+# async def cmd_start(message: types.Message):
+#     kb = [
+#         [types.KeyboardButton(text="С пюрешкой")],
+#         [types.KeyboardButton(text="Без пюрешки")]
+#     ]
+#     keyboard = types.ReplyKeyboardMarkup(keyboard=kb)
+#     await message.answer("Как подавать котлеты?", reply_markup=keyboard)
+
+
+@dp.message(Command("start"))
 async def cmd_start(message: types.Message):
-    await message.answer("Сообщение с <u>HTML-разметкой</u>", parse_mode=ParseMode.HTML)
+    """ Делаем обычные кнопки """
+    kb = [
+        [
+            types.KeyboardButton(text="С пюрешкой"),
+            types.KeyboardButton(text="Без пюрешки")
+        ],
+    ]
+    keyboard = types.ReplyKeyboardMarkup(
+        keyboard=kb,
+        resize_keyboard=True,
+        input_field_placeholder="Выберите способ подачи"
+    )
+    # await message.answer("Как подавать котлеты?", reply_markup=keyboard)
+    await message.answer("Как подавать котлеты?", reply_markup=keyboard)
+
+
+@dp.message(F.text.lower() == "с пюрешкой")
+async def with_puree(message: types.Message):
+    """ Обрабатываем нажатие кнопки и удаляем кнопки. Можно вывести другие кнопки """
+    await message.reply("Отличный выбор!", reply_markup=types.ReplyKeyboardRemove())
+
+@dp.message(F.text.lower() == "без пюрешки")
+async def without_puree(message: types.Message):
+    await message.reply("Так невкусно!", reply_markup=types.ReplyKeyboardRemove())
+
+
+@dp.message(Command("reply_builder"))
+async def reply_builder(message: types.Message):
+    builder = ReplyKeyboardBuilder()
+    for i in range(1, 17):
+        builder.add(types.KeyboardButton(text=str(i)))
+    builder.adjust(4)
+    await message.answer(
+        "Выберите число:",
+        reply_markup=builder.as_markup(resize_keyboard=True),
+    )
+
+
+# У объекта обычной клавиатуры есть ещё две полезных опции:
+# one_time_keyboard для автоматического скрытия кнопок после нажатия и
+# selective для показа клавиатуры лишь некоторым участникам группы.
+# Их использование остаётся для самостоятельного изучения.
+# https://core.telegram.org/bots/api#replykeyboardmarkup
+
+
+@dp.message(Command("special_buttons"))
+async def cmd_special_buttons(message: types.Message):
+    builder = ReplyKeyboardBuilder()
+    # метод row позволяет явным образом сформировать ряд
+    # из одной или нескольких кнопок. Например, первый ряд
+    # будет состоять из двух кнопок...
+    builder.row(
+        types.KeyboardButton(text="Запросить геолокацию", request_location=True),
+        types.KeyboardButton(text="Запросить контакт", request_contact=True)
+    )
+    # ... второй из одной ...
+    builder.row(types.KeyboardButton(
+        text="Создать викторину",
+        request_poll=types.KeyboardButtonPollType(type="quiz"))
+    )
+    # ... а третий снова из двух
+    builder.row(
+        types.KeyboardButton(
+            text="Выбрать премиум пользователя",
+            request_user=types.KeyboardButtonRequestUser(
+                request_id=1,
+                user_is_premium=True
+            )
+        ),
+        types.KeyboardButton(
+            text="Выбрать супергруппу с форумами",
+            request_chat=types.KeyboardButtonRequestChat(
+                request_id=2,
+                chat_is_channel=False,
+                chat_is_forum=True
+            )
+        )
+    )
+    # WebApp-ов пока нет, сорри :(
+
+    await message.answer(
+        "Выберите действие:",
+        reply_markup=builder.as_markup(resize_keyboard=True),
+    )
+
+# две заготовки хэндлеров на приём нажатий от нижних двух кнопок
+
+@dp.message(F.user_shared)
+async def on_user_shared(message: types.Message):
+    print(
+        f"Request {message.user_shared.request_id}. "
+        f"User ID: {message.user_shared.user_id}"
+    )
+
+
+@dp.message(F.chat_shared)
+async def on_user_shared(message: types.Message):
+    print(
+        f"Request {message.chat_shared.request_id}. "
+        f"User ID: {message.chat_shared.chat_id}"
+    )
+
+
+########## InlineKeyboardBuilder
+
+
+@dp.message(Command("inline_url"))
+async def cmd_inline_url(message: types.Message, bot: Bot):
+    builder = InlineKeyboardBuilder()
+    builder.row(types.InlineKeyboardButton(
+        text="GitHub", url="https://github.com")
+    )
+    builder.row(types.InlineKeyboardButton(
+        text="Оф. канал Telegram",
+        url="tg://resolve?domain=telegram")
+    )
+
+    # Чтобы иметь возможность показать ID-кнопку,
+    # У юзера должен быть False флаг has_private_forwards
+    # user_id = 1234567890
+    # chat_info = await bot.get_chat(user_id)
+    # if not chat_info.has_private_forwards:
+    #     builder.row(types.InlineKeyboardButton(
+    #         text="Какой-то пользователь",
+    #         url=f"tg://user?id={user_id}")
+    #     )
+
+    await message.answer(
+        'Выберите ссылку',
+        reply_markup=builder.as_markup(),
+    )
+
+
+@dp.message(Command("random"))
+async def cmd_random(message: types.Message):
+    builder = InlineKeyboardBuilder()
+    builder.add(types.InlineKeyboardButton(
+        text="Нажми меня",
+        callback_data="random_value")
+    )
+    await message.answer(
+        "Нажмите на кнопку, чтобы бот отправил число от 1 до 10",
+        reply_markup=builder.as_markup()
+    )
+
+@dp.callback_query(F.data == "random_value")
+async def send_random_value(callback: types.CallbackQuery):
+    """ Обрабатываем нажатие кнопки """
+    await callback.message.answer(str(randint(1, 10)))
+    await callback.answer(
+        text="Спасибо, что воспользовались ботом!",
+        show_alert=True
+    )
+    # или просто await callback.answer()
+
+# В функции send_random_value мы вызывали метод answer() не у message,
+# а у callback.message. Это связано с тем, что колбэк-хэндлеры работают
+# не с сообщениями (тип Message), а с колбэками (тип CallbackQuery),
+# у которого другие поля, и само сообщение — всего лишь его часть. Учтите также,
+# что message — это сообщение, к которому была прицеплена кнопка (т.е. отправитель
+# такого сообщения — сам бот). Если хотите узнать, кто нажал на кнопку,
+# смотрите поле from (в вашем коде это будет callback.from_user,
+# т.к. слово from зарезервировано в Python)
+
+
+# Здесь хранятся пользовательские данные.
+# Т.к. это словарь в памяти, то при перезапуске он очистится
+user_data = {}
+
+def get_keyboard():
+    buttons = [
+        [
+            types.InlineKeyboardButton(text="-1", callback_data="num_decr"),
+            types.InlineKeyboardButton(text="+1", callback_data="num_incr")
+        ],
+        [types.InlineKeyboardButton(text="Подтвердить", callback_data="num_finish")]
+    ]
+    keyboard = types.InlineKeyboardMarkup(inline_keyboard=buttons)
+    return keyboard
+
+
+async def update_num_text(message: types.Message, new_value: int):
+    await message.edit_text(
+        f"Укажите число: {new_value}",
+        reply_markup=get_keyboard()
+    )
+
+
+# async def update_num_text(message: types.Message, new_value: int):
+#     """ Что бы исключить ошибку """
+#     with suppress(TelegramBadRequest):
+#         await message.edit_text(
+#             f"Укажите число: {new_value}",
+#             reply_markup=get_keyboard()
+#         )
+
+
+@dp.message(Command("numbers"))
+async def cmd_numbers(message: types.Message):
+    user_data[message.from_user.id] = 0
+    await message.answer("Укажите число: 0", reply_markup=get_keyboard())
+
+
+@dp.callback_query(F.data.startswith("num_"))
+async def callbacks_num(callback: types.CallbackQuery):
+    user_value = user_data.get(callback.from_user.id, 0)
+    action = callback.data.split("_")[1]
+
+    if action == "incr":
+        user_data[callback.from_user.id] = user_value+1
+        await update_num_text(callback.message, user_value+1)
+    elif action == "decr":
+        user_data[callback.from_user.id] = user_value-1
+        await update_num_text(callback.message, user_value-1)
+    elif action == "finish":
+        await callback.message.edit_text(f"Итого: {user_value}")
+
+    await callback.answer()
+
+
+
+
+
+
+
+
+
+
+
 
 
 @dp.message(Command("answer"))
